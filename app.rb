603f0a353b8892491584a07460d0645b8dd917d5
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-12-13 03:19:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
-require 'sinatra/base'
+`require 'sinatra/base'
 
 class App < Sinatra::Base
 
+get '/newteam' do
+  @name = params[:name]
+  erb :newteam
+end
 
+post '/team' do
+  @name = "Bballers"
+  erb :team
+end
 end
