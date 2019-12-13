c45622e543460312a298b6b9a022f45ad528be87
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-12-13 03:31:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,10 +10,10 @@ post '/team' do
   @name = params[:name]
   @coach = params[:coach]
   @point_guard = params[:pg]
-  @shooting_guard = params[:shooting_guard]
-  @small_forward = params[:small_forward]
-  @power_forward = params[:power_forward]
-  @center = params[:center]
+  @shooting_guard = params[:sg]
+  @small_forward = params[:sf]
+  @power_forward = params[:pf]
+  @center = params[:c]
   erb :team
 end
 end
