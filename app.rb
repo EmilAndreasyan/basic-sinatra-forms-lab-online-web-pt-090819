6b2c75ef99557d461e9ce31b34edce01e92d15a1
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-12-13 03:46:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,18 +2,17 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-  get '/newteam'
-  
-  erb :newteam
+  get '/newteam' do
+    erb :newteam
   end
-  
+
   post '/team' do
     @name = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
     @sg = params[:sg]
-    @sf = params[:sf]
     @pf = params[:pf]
+    @sf = params[:sf]
     @c = params[:c]
     erb :team
   end
